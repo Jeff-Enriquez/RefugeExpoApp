7122edb4bf53aa98108267b8281fd3969f7c2a6c
Jeff-Enriquez <jeff.t.hawkes@gmail.com>
2020-09-10 16:43:38 -0700

    Add heading

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
+import Heading from './components/Heading'
 
 export default function App() {
   return (
     <SafeAreaView style={styles.safeAreaView}>
       <StatusBar barStyle='light-content' />
+      <Heading/>
       <View style={styles.container}>
         <Text style={styles.text}>Hello</Text>
       </View>
---
+++ components/Heading/index.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Image } from 'react-native-elements';
+import { AppLoading } from 'expo';
+import {
+  useFonts,
+  Montserrat_400Regular as Montserrat400,
+} from '@expo-google-fonts/montserrat';
+
+export default function Heading() {
+  let [fontsLoaded] = useFonts({
+    Montserrat400,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  } else {
+    return (
+      <View style={styles.heading}>
+        <Image source={require('../../assets/images/fire-icon.png')} style={{width: 33, height: 33}}/>
+        <Text style={styles.headingText}>Refuge</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  heading: {
+    flexDirection: 'row',
+    width: '100%',
+    maxHeight: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'black',
+    flex: 1,
+  },
+  headingText: {
+    color: 'white',
+    fontFamily: 'Montserrat400',
+    fontSize: 14,
+  },
+});
